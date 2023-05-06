6277606a3bbe6f212adecea8b5539b36d1ab9c3a
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2023-05-06 14:32:20 -0500

    more data issues

--- visualizations/may23-election/election_data_processing.py
+++ visualizations/may23-election/election_data_processing.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 
 
 precincts = gpd.read_file('election_pcts_coa.geojson',index_col='pct')
-precincts.set_index("PCTCODE")
+precincts = precincts.set_index("PCTCODE")
 data = pd.read_csv('mayor-runoff.csv',index_col='pct')
 pct_merge = precincts.merge(data, left_index=True, right_index=True,how='left')
 
