81317c0288287df2d04639f8cacd8db8fff1e95f
Charlie Henry <30810522+Charlie-Henry@users.noreply.github.com>
2023-05-06 14:30:30 -0500

    broken data

--- visualizations/may23-election/election_data_processing.py
+++ visualizations/may23-election/election_data_processing.py
@@ -2,11 +2,14 @@ import pandas as pd
 import geopandas as gpd
 
 
-precincts = gpd.read_file('Travis_County_elections_results_may23.geojson',index_col='pct')
+precincts = gpd.read_file('election_pcts_coa.geojson',index_col='pct')
+precincts.set_index("PCTCODE")
 data = pd.read_csv('mayor-runoff.csv',index_col='pct')
 pct_merge = precincts.merge(data, left_index=True, right_index=True,how='left')
 
-pct_merge["for_pct"] = pct_merge["For"]/pct_merge["Total"]
-pct_merge["against_pct"] = pct_merge["Against"]/pct_merge["Total"]
+pct_merge["A_for_pct"] = pct_merge["A-For"]/pct_merge["A-Total"]
+pct_merge["A_against_pct"] = pct_merge["A-Against"]/pct_merge["A-Total"]
+pct_merge["B_for_pct"] = pct_merge["B-For"]/pct_merge["B-Total"]
+pct_merge["B_against_pct"] = pct_merge["B-Against"]/pct_merge["B-Total"]
 
 pct_merge.to_file("may-23-election.geojson", driver="GeoJSON")
